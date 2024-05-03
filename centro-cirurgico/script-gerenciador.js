63a53be818851a1dfa65273916c364451633b821
Valdeli de Jesus <valderbill@gmail.com>
2024-05-02 21:50:24 -0300

    Centro Cirurgico continuação

--- centro-cirurgico/script-gerenciador.js
+++ centro-cirurgico/script-gerenciador.js
@@ -1,9 +1,17 @@
+document.getElementById("bt-gravar").addEventListener("click", gravar);
+
+
 let lista = [];
 
 function gravar() {
     let obj = {};
     obj.nome = document.getElementById("nome").value;
     obj.status = document.getElementById("status").value;
+    obj.local = document.getElementById("local").value;
+    obj.inPrevisto = document.getElementById("inPrevisto").value;
+    obj.inCirurgia = document.getElementById("inCirurgia").value;
+    obj.fimCirurgia = document.getElementById("fimCirurgia").value;
+    obj.saidaPrevista = document.getElementById("saidaPrevista").value;
     
     lista.push(obj);
     atulizarTabela();
@@ -12,11 +20,10 @@ function gravar() {
 function atulizarTabela() {
     let tbody = "";
     for (const pessoa of lista) {
-        tbody += `<tr><td>${pessoa.nome}</td><td>${pessoa.status}</td></tr>`;
+        tbody += `<tr><td>${pessoa.nome}</td><td>${pessoa.status}</td><td>${pessoa.local}</td><td>${pessoa.inPrevisto}</td><td>${pessoa.inCirurgia}</td><td>${pessoa.fimCirurgia}</td><td>${pessoa.saidaPrevista}</td></tr>`;
    
     }   
     document.getElementById("tbody").innerHTML = tbody;
 }
-document.getElementById("bt-gravar").addEventListener("click", gravar);
 
 
