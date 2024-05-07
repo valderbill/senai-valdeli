20aa2eb91e4b362f0468eb731d8f223276603670
Valdeli de Jesus <valderbill@gmail.com>
2024-05-06 21:50:26 -0300

    Centro Cirurgico Continuação 3

--- centro-cirurgico/script-gerenciador.js
+++ centro-cirurgico/script-gerenciador.js
@@ -1,5 +1,6 @@
 document.getElementById("bt-gravar").addEventListener("click", gravar);
-
+document.getElementById("bt-apagar").addEventListener("click", apagar);
+document.getElementById("bt-novo").addEventListener("click", novo);
 
 let lista = [];
 
@@ -14,16 +15,33 @@ function gravar() {
     obj.saidaPrevista = document.getElementById("saidaPrevista").value;
     
     lista.push(obj);
-    atulizarTabela();
+    atualizarTabela();
 }
 
-function atulizarTabela() {
+function atualizarTabela() {
     let tbody = "";
     for (const pessoa of lista) {
         tbody += `<tr><td>${pessoa.nome}</td><td>${pessoa.status}</td><td>${pessoa.local}</td><td>${pessoa.inPrevisto}</td><td>${pessoa.inCirurgia}</td><td>${pessoa.fimCirurgia}</td><td>${pessoa.saidaPrevista}</td></tr>`;
-   
-    }   
+    }
     document.getElementById("tbody").innerHTML = tbody;
 }
 
+function apagar() {
+    lista = [];
+    atualizarTabela();
+}
 
+function apagarItem(index) {
+    lista.splice(index, 1);
+    atualizarTabela();
+}
+
+function novo() {
+    document.getElementById("nome").value = "";
+    document.getElementById("status").value = "";
+    document.getElementById("local").value = "";
+    document.getElementById("inPrevisto").value = "";
+    document.getElementById("inCirurgia").value = "";
+    document.getElementById("fimCirurgia").value = "";
+    document.getElementById("saidaPrevista").value = "";
+}
