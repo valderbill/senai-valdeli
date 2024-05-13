4c0c3ae89b3ccf554ca2823000dc432ab50ca66c
Valdeli de Jesus <valderbill@gmail.com>
2024-05-13 20:46:08 -0300

    update

--- jogo-mamoria/js/script.js
+++ jogo-mamoria/js/script.js
@@ -7,13 +7,23 @@ for (let i = 0; i < tamanho; i++) {
     lsEmoticons.push(lsEmoticons[i])    
 }
 // para cada bloco incluir o evento de click
+let blocos = {};
 let lsBloco = document.getElementsByClassName("bloco");
+let id = 0;
 for (const b of lsBloco) {
     b.addEventListener("click", ()=>{
         mostrar(b);
     });
+    icon = lsEmoticons.splice(sortear(),1);
+    blocos[`id${id}`] = icon;
+    b.id = `id${id}`;
+    id++;
 }
 
 function mostrar(b){
-    b.innerHTML = "😊";
+    b.innerHTML = blocos[b.id];
+}
+
+function sortear(){
+    return Math.floor(Math.random() * lsEmoticons.length);
 }
