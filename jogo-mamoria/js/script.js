b811838b52866ac672864e2ecbe5732ff0bc87a7
Valdeli de Jesus <valderbill@gmail.com>
2024-05-13 21:44:27 -0300

    funcionalidade dospares

--- jogo-mamoria/js/script.js
+++ jogo-mamoria/js/script.js
@@ -1,29 +1,52 @@
 // lista dos itens que estarão no jogo
-let lsEmoticons = ["😀","😎","😴","😍","🤯","🤑","🤣","😭"];
+let lsEmoticons = ["😀", "😎", "😴", "😍", "🤯", "🤑", "🤣", "😭","👀","✔","🎶","🎁","💖","👳‍♂️","🎄","🛴"];
 
 // replicar os itens na tabela
 const tamanho = lsEmoticons.length;
 for (let i = 0; i < tamanho; i++) {
-    lsEmoticons.push(lsEmoticons[i])    
+    lsEmoticons.push(lsEmoticons[i])
 }
 // para cada bloco incluir o evento de click
 let blocos = {};
 let lsBloco = document.getElementsByClassName("bloco");
 let id = 0;
 for (const b of lsBloco) {
-    b.addEventListener("click", ()=>{
+    b.addEventListener("click", () => {
         mostrar(b);
     });
-    icon = lsEmoticons.splice(sortear(),1);
+    icon = lsEmoticons.splice(sortear(), 1);
     blocos[`id${id}`] = icon;
     b.id = `id${id}`;
     id++;
 }
+let aberto = ["", ""];
+function mostrar(b) {
+    //b.innerHTML = blocos[b.id];
+    if (b.innerHTML != "") return;
+    if (aberto[0] != "" && aberto[1] != "") testar();
 
-function mostrar(b){
-    b.innerHTML = blocos[b.id];
+    if (aberto[0] == "") {
+        aberto[0] = b;
+        b.innerHTML = blocos[b.id];
+        return;
+    }
+    if (aberto[1] == "") {
+        aberto[1] = b;
+        b.innerHTML = blocos[b.id];
+        return;
+    }
+
+}
+function testar() {
+    if (aberto[0].innerHTML != aberto[1].innerHTML) {
+        aberto[0].innerHTML = "";
+        aberto[1].innerHTML = "";
+    }
+    aberto[0] = "";
+    aberto[1] = "";
 }
 
-function sortear(){
+
+function sortear() {
     return Math.floor(Math.random() * lsEmoticons.length);
 }
