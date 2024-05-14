dbfc36c0e6d22303123a01567c5d09236c31518b
Valdeli de Jesus <valderbill@gmail.com>
2024-05-13 21:52:43 -0300

    quantidade flexivel

--- jogo-mamoria/js/script.js
+++ jogo-mamoria/js/script.js
@@ -1,10 +1,14 @@
 // lista dos itens que estarão no jogo
-let lsEmoticons = ["😀", "😎", "😴", "😍", "🤯", "🤑", "🤣", "😭","👀","✔","🎶","🎁","💖","👳‍♂️","🎄","🛴"];
+let lsEmoticons = ["😀", "😎", "😴", "😍", "🤯", "🤑", "🤣", "😭","🚑","🏍"];
 
 // replicar os itens na tabela
 const tamanho = lsEmoticons.length;
 for (let i = 0; i < tamanho; i++) {
     lsEmoticons.push(lsEmoticons[i])
+}
+const campo = document.getElementById("campo");
+for (const key in lsEmoticons){
+    campo.innerHTML += `<div class="bloco"></div>`;
 }
 // para cada bloco incluir o evento de click
 let blocos = {};
