17e98ef0625ffff38bd1824f7bf3b768953e1a90
Valdeli de Jesus <valderbill@gmail.com>
2024-05-16 21:39:05 -0300

    jogo caça palavras

---
+++ jogo-descubra a palavra/script.js
@@ -0,0 +1,24 @@
+const frm = document.querySelector("form");
+frm.addEventListener("submit", (e) => { 
+    e.preventDefault(); 
+
+    const palavra = frm.inPalavra.value.trim();
+    const dica = frm.inDica.value;
+    if (palavra.includes(" ")) {
+        alert("Informe uma palavra válida (sem espaços)");
+        frm.inPalavra.focus();
+        return;
+    }
+    if (localStorage.getItem("jogoPalavra")) {
+        localStorage.setItem("jogoPalavra", localStorage.getItem("jogoPalavra") + ";" + palavra); 
+        localStorage.setItem("jogoDica", localStorage.getItem("jogoDica") + ";" + dica); 
+    } else {
+        localStorage.setItem("jogoPalavra", palavra);
+        localStorage.setItem("jogoDica", dica);
+    }
+    if (localStorage.getItem("jogoPalavra")) {
+        alert(`Palavra ${palavra} Cadastrada com Sucesso`);
+    }
+    frm.reset(); 
+    frm.inPalavra.focus(); 
+});
