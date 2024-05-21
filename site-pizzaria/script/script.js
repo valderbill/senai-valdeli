b21dbabd7b852611dba882811a1e934ad1f6376b
Valdeli de Jesus <valderbill@gmail.com>
2024-05-21 20:02:18 -0300

    Carregar Produtos atraces de um lista json

--- site-pizzaria/script/script.js
+++ site-pizzaria/script/script.js
@@ -7,3 +7,18 @@ menuToggle.addEventListener('click', () =>{
     menuContent.classList.toggle('on', show);
     show = !show;
 })
+let itensCardapio = document.querySelector(".itens-cardapio");
+for (const p of produtos) {
+    itensCardapio.innerHTML += `
+    <div class="">
+        <img src="${p.img}" alt="${p.dsImg}">
+        <div class="info">
+            <h3>${p.nome}</h3>
+            <h4>6 fatias<span>R$${p.fatias6}</span></h4>
+            <h4>8 fatias<span>R$${p.fatias8}</span></h4>
+            <h4>12 fatias<span>R$${p.fatias12}</span></h4>
+            <button class="pedir">pedir agora</button>
+        </div>
+    </div>
+    `;
+}
