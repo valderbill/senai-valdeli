98f339e31f9811cc39ce700c9a6e72136a855060
Valdeli de Jesus <valderbill@gmail.com>
2024-05-23 20:30:50 -0300

    Atualizar tabela e valor total dos produtos selecionados

--- site-pizzaria/script/script.js
+++ site-pizzaria/script/script.js
@@ -8,6 +8,7 @@ menuToggle.addEventListener('click', () =>{
     show = !show;
 })
 let itensCardapio = document.querySelector(".itens-cardapio");
+let id = 0;
 for (const p of produtos) {
     itensCardapio.innerHTML += `
     <div class="">
@@ -17,10 +18,11 @@ for (const p of produtos) {
             <h4>6 fatias<span>R$${p.fatias6}</span></h4>
             <h4>8 fatias<span>R$${p.fatias8}</span></h4>
             <h4>12 fatias<span>R$${p.fatias12}</span></h4>
-            <button class="pedir">pedir agora</button>
+            <button id="id${id}" class="pedir">pedir agora</button>
         </div>
     </div>
     `;
+    id++;
 }
 let telaCarrinho = document.querySelector('.tela-carrinho');
 let continuar = document.querySelector('.continuar');
@@ -30,4 +32,36 @@ continuar.addEventListener('click',()=>{
 let compras = document.querySelector('.compras');
 compras.addEventListener("click",()=>{
     telaCarrinho.classList.toggle('ocultar-tela-carrinho');
+    menuToggle.click(); // fechar menu versão mibile
 } )
+let lsPedido = document.querySelectorAll('.pedir');
+for (const bt of lsPedido) {
+    bt.addEventListener('click', ()=>{
+        let id = bt.id.replace('id','');
+        produtos[id].quantidade = 1;
+        atualizarTabela();
+    });
+}
+let tbody = document.querySelector('tbody');
+function atualizarTabela() {
+    tbody.innerHTML = '';
+    let total = 0;
+    for (const p of produtos) {
+        if (p.quantidade > 0) {
+            tbody.innerHTML += `
+                        <tr>
+                        <td>${p.nome}</td>
+                        <td>${p.quantidade}x${p.fatias8}=${p.quantidade*p.fatias8}(8 fatias)</td>
+                        <td>
+                            <i class="bi bi-plus-square-fill"></i>
+                            <i class="bi bi-dash-square-fill"></i>
+                        </td>
+                    </tr>`;
+        total += p.quantidade*p.fatias8;
+            
+        }
+    }
+    document.querySelector('#total-pedido').innerHTML =`Valor total do pedido = R$${total}`;
+}
+
+
